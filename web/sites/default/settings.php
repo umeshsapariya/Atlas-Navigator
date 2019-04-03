4deaa4bbabc5ca44fb03be7348f64a48798d15ab
Umesh Sapariya <umeshsapariya@gmail.com>
2019-04-03 22:37:48 +0530

    Multisite configuration

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -34,7 +34,7 @@ $config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync';
 
 // Automatic Platform.sh settings.
 if (file_exists($app_root . '/' . $site_path . '/../settings.platformsh.php')) {
-  $platformsh_subsite_id = 'atlas';
+  $platformsh_subsite_id = 'login';
   include $app_root . '/' . $site_path . '/../settings.platformsh.php';
 }
 
